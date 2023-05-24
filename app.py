af2950372d530fe860a684a21426e510b77df65c
stibiumghost <116355639+stibiumghost@users.noreply.github.com>
2023-05-24 22:58:48 +0500

    Add a scoring system and layout improvements

--- app.py
+++ app.py
@@ -5,24 +5,51 @@ from text_gen import generate_text
 
 
 def get_answer(question, answer, options):
+    '''File given answer'''
+
     global questions
     global SEPARATOR
     answer = options.split(SEPARATOR)[answer]
-    answers.update({question:int(answer == questions.Answer[questions.Question == question].iat[0])})
+    questions['Choice'][questions.Question == question] = answer
+
+
+def find_score(column_name):
+    '''Count chosen answers in a given column'''
+
+    global questions
+    actual = questions[column_name].to_list()
+    chosen = questions.Choice.to_list()
+    return sum(a == c for a, c in zip(actual, chosen))
 
 
 def set_score():
-    global answers
-    return str(sum(answers.values()))
+    '''Return the score (for the human and for the models)'''
+
+    global NUM_QUESTIONS
+    score = find_score(answers[0])
+    ai_score = {name:find_score(name) for name in answers[1:]}
+
+    start = '## <p style="text-align: center;">'
+    end = '</p>'
+
+    if       score == NUM_QUESTIONS:         text = f'Perfect score!'
+    else:
+        if   score == 0:                     text = 'Not a single right answer. Are you doing this on purpose?'
+        elif score <= NUM_QUESTIONS / 2 + 1: text = f'Only {score} right answers out of {NUM_QUESTIONS}. You ought to pay more attention.'
+        else:                                text = f'{score} right answers out of {NUM_QUESTIONS}. It\'s probably alright.'
+        for name in ai_score.keys():
+            text += f'\n{name} got {ai_score[name]}.'
+
+    return start + text + end
 
 
-NUM_QUESTIONS = 6
+NUM_QUESTIONS = 10
 SEPARATOR = '<[._.]>'
 
 
 # Adding/replacing models may require adjustments to text_gen.py
-model_names = ['microsoft/GODEL-v1_1-base-seq2seq',
-               'facebook/blenderbot-400M-distill',
+model_names = ['microsoft/GODEL-v1_1-large-seq2seq',
+               'facebook/blenderbot-1B-distill',
                'facebook/blenderbot_small-90M']
 
 tokenizers = [transformers.AutoTokenizer.from_pretrained(model_names[0]),
@@ -57,30 +84,43 @@ for i in range(len(model_names)):
                                           minimum=len(questions.Answer[questions.Question == x].iat[0].split())+8))
 
 
-cols = ['Answer'] + model_names
-answers = {}
+questions['Choice'] = ''
+answers = ['Answer'] + model_names
 
 
+# App
 with gr.Blocks() as test_gen:
+    with gr.Row():
 
-    for ind, question in enumerate(questions.Question.to_list(), start=1):
-        letters = list('ABCD')
-        options = list(set([questions[col][questions.Question == question].iat[0] for col in cols]))
+        with gr.Column(scale=1):
+            pass
 
-        gr.Markdown(f'### <p>{ind}. {question}</p>')
+        with gr.Column(scale=2):
 
-        for letter, option in zip(letters, options):
-            gr.Markdown(f'{letter}. {option}')
+            gr.Markdown(f'## <p style="text-align: center;">IMITATION GAME</p>\n' +
+                        f'### <p style="text-align: center;">Choose answers NOT given by an AI model.')
 
-        options = gr.State(SEPARATOR.join(options))
+            for ind, question in enumerate(questions.Question.to_list(), start=1):
+                letters = list('ABCD')
+                options = list(set([questions[col][questions.Question == question].iat[0] for col in answers]))
 
-        radio = gr.Radio(letters, type='index', show_label=False)
-        question = gr.State(question)
-        radio.change(fn=get_answer, inputs=[question, radio, options])
+                gr.Markdown(f'### <p>{ind}. {question}</p>')
 
-    button = gr.Button(value='Get score')
-    score = gr.Markdown()
-    button.click(fn=set_score, outputs=score)
+                for letter, option in zip(letters, options):
+                    gr.Markdown(f'{letter}. {option}')
+
+                options = gr.State(SEPARATOR.join(options))
+
+                radio = gr.Radio(letters, type='index', show_label=False)
+                question = gr.State(question)
+                radio.change(fn=get_answer, inputs=[question, radio, options])
+
+            button = gr.Button(value='Get score')
+            score = gr.Markdown()
+            button.click(fn=set_score, outputs=score)
+
+        with gr.Column(scale=1):
+            pass
 
 
 test_gen.launch()
