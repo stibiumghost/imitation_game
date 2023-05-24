5d2299b130425fc71307bc3210dd8eb5d3ed0427
stibiumghost <116355639+stibiumghost@users.noreply.github.com>
2023-05-24 22:52:26 +0500

    Update app.py with text generation functionality

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
+import transformers
 import gradio as gr
 import pandas as pd
+from text_gen import generate_text
 
 
-NUM_QUESTIONS = 10
+def get_answer(question, answer, options):
+    global questions
+    global SEPARATOR
+    answer = options.split(SEPARATOR)[answer]
+    answers.update({question:int(answer == questions.Answer[questions.Question == question].iat[0])})
 
 
+def set_score():
+    global answers
+    return str(sum(answers.values()))
+
+
+NUM_QUESTIONS = 6
+SEPARATOR = '<[._.]>'
+
+
+# Adding/replacing models may require adjustments to text_gen.py
+model_names = ['microsoft/GODEL-v1_1-base-seq2seq',
+               'facebook/blenderbot-400M-distill',
+               'facebook/blenderbot_small-90M']
+
+tokenizers = [transformers.AutoTokenizer.from_pretrained(model_names[0]),
+              transformers.BlenderbotTokenizer.from_pretrained(model_names[1]),
+              transformers.BlenderbotSmallTokenizer.from_pretrained(model_names[2])]
+
+model = [transformers.AutoModelForSeq2SeqLM.from_pretrained(model_names[0]),
+         transformers.BlenderbotForConditionalGeneration.from_pretrained(model_names[1]),
+         transformers.BlenderbotSmallForConditionalGeneration.from_pretrained(model_names[2])]
+
+
+# Semi-randomly choose questions
 questions = pd.read_csv('lines_2.txt', sep='*')
 questions = pd.concat([questions[questions.Part == 'Start'].sample(1),
                        questions[questions.Part == 'Middle_1'].sample(1),
@@ -15,38 +45,42 @@ questions = pd.concat([questions[questions.Part == 'Start'].sample(1),
                        ).fillna('')
 
 
-questions['Choice'] = ''
-cols = ['Answer'] + list('ABC')
+# Generate answers
+for i in range(len(model_names)):
+    questions[model_names[i]] = questions.Question.apply(
+                                lambda x: generate_text(
+                                          x,
+                                          questions.Context[questions.Question == x].iat[0],
+                                          model_names[i],
+                                          model[i],
+                                          tokenizers[i],
+                                          minimum=len(questions.Answer[questions.Question == x].iat[0].split())+8))
+
+
+cols = ['Answer'] + model_names
 answers = {}
 
 
-for col in 'ABC':
-    questions[col] = questions.Answer.apply(lambda x: f'{col} {x}')
+with gr.Blocks() as test_gen:
 
+    for ind, question in enumerate(questions.Question.to_list(), start=1):
+        letters = list('ABCD')
+        options = list(set([questions[col][questions.Question == question].iat[0] for col in cols]))
 
-def get_answer(question, answer):
-    global questions
-    global answers
-    answers.update({question:int(answer == questions.Answer[questions.Question == question].iat[0])})
+        gr.Markdown(f'### <p>{ind}. {question}</p>')
 
+        for letter, option in zip(letters, options):
+            gr.Markdown(f'{letter}. {option}')
 
-def set_score():
-    global answers
-    return str(sum(answers.values()))
+        options = gr.State(SEPARATOR.join(options))
 
-
-with gr.Blocks() as demo:
-    for question in questions.Question.to_list():
-        radio = gr.Radio(list(set(
-                        [questions[col][questions.Question == question].iat[0] for col in cols])),
-                        label=question)
+        radio = gr.Radio(letters, type='index', show_label=False)
         question = gr.State(question)
-        radio.change(fn=get_answer, inputs=[question, radio])
+        radio.change(fn=get_answer, inputs=[question, radio, options])
 
     button = gr.Button(value='Get score')
     score = gr.Markdown()
     button.click(fn=set_score, outputs=score)
 
 
-if __name__ == "__main__":
-    demo.launch()
+test_gen.launch()
